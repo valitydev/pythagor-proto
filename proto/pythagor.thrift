321031dbffa91e337953cc0500227f1681215e0c
dmitriibaikov <bdv@empayre.com>
2024-07-23 19:12:44 +0300

    Add proto [2]

--- proto/pythagor.thrift
+++ proto/pythagor.thrift
@@ -8,29 +8,45 @@ typedef i64 Value
 
 exception CalculationNotFound {}
 
+struct CalculationRequest {
+    /* Наименование ИД подсчета */
+    1: required CalculationName calculation_name
+
+    /* ИД операции изменения значения */
+    2: required OperationId operation_id
+
+    /* Добавляемое значение */
+    3: required Value value
+}
+
 struct CalculationResponse {
+    /* Внутренний ИД подсчета */
     1: required CalculationId id
+
+    /* Наименование ИД подсчета */
     2: required CalculationName calculation_name
+
+    /* ИД операции изменения значения */
     3: required OperationId operation_id
+
+    /* Закоммиченное значение */
     4: required Value commit_value
+
+    /* Захолдирование значение */
     5: required Value hold_value
 }
 
 service PythagorService {
 
-    /* Добавить и применить значение */
-    CalculationResponse add(CalculationName calculation_name, OperationId operation_id, Value value)
-
     /* Добавить значение */
-    CalculationResponse hold(CalculationName calculation_name, OperationId operation_id, Value value)
-        throws (1: CalculationNotFound ex1)
+    CalculationResponse hold(CalculationRequest request)
 
     /* Применить значение */
-    CalculationResponse commit(CalculationName calculation_name, OperationId operation_id, Value value)
-        throws (1: CalculationNotFound ex1)
+    CalculationResponse commit(CalculationRequest request) throws (1: CalculationNotFound ex1)
 
     /* Отменить добавление */
-    CalculationResponse rollback(CalculationName calculation_name, OperationId operation_id, Value value)
-        throws (1: CalculationNotFound ex1)
+    CalculationResponse rollback(CalculationRequest request) throws (1: CalculationNotFound ex1)
 
+    /* Добавить и применить значение */
+    CalculationResponse add(CalculationRequest request)
 }
